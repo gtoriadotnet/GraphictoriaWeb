eaf42eed7effafddc44c5eb11580ee8f3fc68ec4
gtoriadotnet <xlxi@gtoria.net>
2021-12-25 19:23:01 -0500

    fix cors

--- web/app/Http/Middleware/Cors.php
+++ web/app/Http/Middleware/Cors.php
@@ -34,7 +34,7 @@ class Cors
 					->setStatusCode(204)
 					->header('Access-Control-Allow-Origin', $allowedOrigin)
 					->header('Access-Control-Allow-Methods', '*')
-					->header('Access-Control-Allow-Headers', '*')
+					->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'))
 					->header('Access-Control-Allow-Credentials', 'true')
 					->header('Access-Control-Max-Age', '86400');
 		}
