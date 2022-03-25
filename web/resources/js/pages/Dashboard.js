a70bd470f52e69e8e5144f7feb62ae3ab8093118
gtoriadotnet <xlxi@gtoria.net>
2022-03-25 01:54:48 -0400

    dashboard changes finish later aaaa

--- web/resources/js/pages/Dashboard.js
+++ web/resources/js/pages/Dashboard.js
@@ -32,17 +32,42 @@ const Dashboard = (props) => {
 			?
 			<Loader />
 			:
-			<div className={`row`}>
-                <div className={`col`}>
-                    <Card className={`justify-content-center`} padding={true}>
-                        <CardTitle>Hello, {user.username}!</CardTitle>
-                        <p>[Avatar goes here]</p>
-                    </Card>
-                </div>
-                <div className={`col justify-content-center`}>
-                    <p>Lmao</p>
-                </div>
-            </div>
+			<div className='container'>
+				<h4>Hello, {user.username}!</h4>
+				<div className='row'>
+					{/* User image, blog, friends */}
+					<div className='col-3'>
+						<div className='card text-center'>
+							<p className='pt-3 px-3'>"{ user.about }"</p>
+							<img src='/images/testing/avatar.png' className='img-fluid' />
+						</div>
+						<div className='card mt-3'>
+							<div className='card-body p-2 text-center'>
+								<h6 className='card-title'>Blog</h6>
+								<hr className='mx-5 my-0 mb-2' />
+								<ul className="text-center list-unstyled mb-1">
+									<li className="pb-2"><a href="#" className="text-decoration-none fw-normal"><i className="fa-solid fa-circle-right"></i> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</a></li>
+									<li className="pb-2"><a href="#" className="text-decoration-none fw-normal"><i className="fa-solid fa-circle-right"></i> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</a></li>
+									<li className="pb-2"><a href="#" className="text-decoration-none fw-normal"><i className="fa-solid fa-circle-right"></i> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</a></li>
+								</ul>
+								<div className="text-left px-2">
+									<a href="#" className="text-decoration-none fw-normal">More <i className="fa-solid fa-caret-right"></i></a>
+								</div>
+							</div>
+						</div>
+					</div>
+					
+					{/* Feed */}
+					<div className='col-7'>
+					
+					</div>
+					
+					{/* Recently Played */}
+					<div className='col-2'>
+					
+					</div>
+				</div>
+			</div>
 		);
 }
 
