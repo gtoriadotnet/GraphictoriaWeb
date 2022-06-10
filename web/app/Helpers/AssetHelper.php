079513157c6396fba11c863f3323093c61d08f36
Graphictoria <XlXi@gtoria.net>
2022-06-09 23:22:59 -0400

    Basic asset helper (still need to finish assets later but im tired so im going to bed)

---
+++ web/app/Helpers/AssetHelper.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+	Graphictoria 2022
+	Asset Helper
+*/
+
+namespace App\Helpers;
+
+class AssetHelper
+{
+	public static $assetTypes = [
+		[0]  = 'Image',
+		[1]  = 'TShirt',
+		[2]  = 'Audio',
+		[3]  = 'Mesh',
+		[4]  = 'Lua',
+		[5]  = 'HTML',
+		[6]  = 'Text',
+		[7]  = 'Hat',
+		[8]  = 'Place',
+		[9]  = 'Model',
+		[10] = 'Shirt',
+		[11] = 'Pants',
+		[12] = 'Decal',
+		[13] = 'Avatar',
+		[14] = 'Head',
+		[15] = 'Face',
+		[16] = 'Gear',
+		[17] = 'Badge',
+		[18] = 'GroupEmblem',
+		[19] = 'Animation',
+		[20] = 'Arms',
+		[21] = 'Legs',
+		[22] = 'Torso',
+		[23] = 'RightArm',
+		[24] = 'LeftArm',
+		[25] = 'LeftLeg',
+		[26] = 'RightLeg',
+		[27] = 'Package',
+		[28] = 'YouTubeVideo',
+		[29] = 'GamePass',
+		[30] = 'App',
+		[31] = 'Code',
+		[32] = 'Plugin',
+		[33] = 'SolidModel',
+		[34] = 'MeshPart',
+		[35] = 'HairAccessory',
+		[36] = 'FaceAccessory',
+		[37] = 'NeckAccessory',
+		[38] = 'ShoulderAccessory',
+		[39] = 'FrontAccessory',
+		[40] = 'BackAccessory',
+		[41] = 'WaistAccessory',
+		[42] = 'ClimbAnimation',
+		[43] = 'DeathAnimation',
+		[44] = 'FallAnimation',
+		[45] = 'IdleAnimation',
+		[46] = 'JumpAnimation',
+		[47] = 'RunAnimation',
+		[48] = 'SwimAnimation',
+		[49] = 'WalkAnimation',
+		[50] = 'PoseAnimation',
+		[51] = 'LocalizationTableManifest',
+		[52] = 'LocalizationTableTranslation',
+		[53] = 'EmoteAnimation',
+		[54] = 'Video',
+		[55] = 'TexturePack'
+	];
+}
